e21851dce6bd18990012e015d1a144f1fccc206f
yunna <homer_j@hanmail.net>
2023-04-25 19:16:51 +0900

    preventDefault 이해를 위한 a 태그 예시

---
+++ app.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const link = document.querySelector('a');
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+function onLinkClick(e) {
+  e.preventDefault();
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
+link.addEventListener('click', onLinkClick);
