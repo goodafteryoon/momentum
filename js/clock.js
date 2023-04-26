eaa22d735e1a6a9a1b7473b039d4bdc6fbfdac2b
yunna <homer_j@hanmail.net>
2023-04-26 20:16:57 +0900

    refact : 시계 객체 변수 사용해서 Refactor

--- js/clock.js
+++ js/clock.js
@@ -3,23 +3,27 @@ const clock = document.querySelector('h2#clock');
 function getClock() {
   const date = new Date();
   const fullClock = {
-    hour: '',
-    min: '',
-    sec: '',
+    hour: date.getHours(),
+    min: date.getMinutes(),
+    sec: date.getSeconds(),
   };
 
-  fullClock.hour = date.getHours();
-
-  if (date.getMinutes() < 10) {
-    fullClock.min = `0${date.getMinutes()}`;
+  if (fullClock.hour < 10) {
+    fullClock.hour = `0${fullClock.hour}`;
   } else {
-    fullClock.min = date.getMinutes();
+    fullClock.hour;
   }
 
-  if (date.getSeconds() < 10) {
-    fullClock.sec = `0${date.getSeconds()}`;
+  if (fullClock.min < 10) {
+    fullClock.min = `0${fullClock.min}`;
   } else {
-    fullClock.sec = date.getSeconds();
+    fullClock.min;
+  }
+
+  if (fullClock.sec < 10) {
+    fullClock.sec = `0${fullClock.sec}`;
+  } else {
+    fullClock.sec;
   }
 
   clock.innerText = `${fullClock.hour}:${fullClock.min}:${fullClock.sec}`;
