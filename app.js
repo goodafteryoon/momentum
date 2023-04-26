297e83036f4efbc790b3b65277bd5bb0182eb834
yunna <homer_j@hanmail.net>
2023-04-26 18:18:13 +0900

    feat : greeting 페이지

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const link = document.querySelector('a');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // console.log(username);
+  localStorage.setItem('username', username);
+  paintGreeting(username);
 }
 
-function onLinkClick(e) {
-  e.preventDefault();
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', onLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
