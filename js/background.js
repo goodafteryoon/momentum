a2af5238a21f07501f36b4eadb6d7a84d4d4f981
yunna <homer_j@hanmail.net>
2023-04-30 00:24:17 +0900

    add : 랜덤 배경 이미지

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const imgs = [
+  'homer1.jpg',
+  'homer2.jpg',
+  'homer3.jpg',
+  'homer4.jpg',
+  'homer5.jpg',
+  'homer6.jpg',
+];
+
+const backgroundImg = document.createElement('img');
+console.log(backgroundImg);
+
+const randomImg = imgs[Math.floor(Math.random() * imgs.length)];
+
+backgroundImg.src = `img/${randomImg}`;
+
+document.body.appendChild(backgroundImg);
