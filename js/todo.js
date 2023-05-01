f8905ae3b8da4a87e71fc3fbdc2d23f66838c2f9
yunna <homer_j@hanmail.net>
2023-05-01 21:12:22 +0900

    add : todo 지우기 기능
    - event.target으로 현재 이벤트가 발생하는(버튼 클릭) 요소를 선택한 뒤,
    - 그의 부모인 리스트 전체를 지워줌 (li.remove로)

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+function handleToDoDelete(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
+  const button = document.createElement('button');
   li.appendChild(span);
   span.innerText = newTodo;
+  button.innerText = '❌';
+  button.addEventListener('click', handleToDoDelete);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
