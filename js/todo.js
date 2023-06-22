1c01ea473b650e7537bfef13f04438190ecf7f8c
yunna <homer_j@hanmail.net>
2023-06-22 17:25:17 +0900

    feat : 로컬 스토리지에 저장된 todo 삭제 기능

--- js/todo.js
+++ js/todo.js
@@ -13,17 +13,19 @@ function saveToDos() {
 function handleToDoDelete(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); // 내가 클릭한 li.id와 다른 toDo는 남겨두는 것
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
   const span = document.createElement('span');
-  const button = document.createElement('button');
-  li.appendChild(span);
-
   span.innerText = newTodo.text;
+  const button = document.createElement('button');
   button.innerText = '❌';
   button.addEventListener('click', handleToDoDelete);
+  li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
 }
@@ -50,3 +52,8 @@ if (savedToDos !== null) {
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
 }
+
+/**
+ * 특정 item을 삭제할 때는 그 item을 삭제하는 것이 아니라
+ * 특정 item을 제외한 새로운 item 목록을 렌더하는 것
+ *  */
